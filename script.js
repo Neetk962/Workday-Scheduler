8415202670ce8aa894017f28b464033d143150f3
Ravneet Kaur <neet.k962@gmail.com>
2023-10-04 09:41:58 -0700

    /added jquery

--- script.js
+++ script.js
@@ -1,7 +1,118 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+$(document).ready(function(){
+//get current day using dayjs
+var currentday=dayjs().format('dd,MM,D,YYYY');
+$('#currentDay').text(currentday)
+
+
+var cuurenttime=dayjs().format('HH');
+$()
+console.log(cuurenttime)
+
+
+
+$( ".row" ).each(function( index ) {
+    
+    var rowtime= parseInt($(this).attr('id'),10)
+    $(this).children('textarea').val(localStorage.getItem(rowtime))
+
+    if(rowtime == cuurenttime) {
+        $(this).addClass("present")
+        $(this).removeClass("past")
+        $(this).removeClass("future")
+
+    }
+    if(rowtime > cuurenttime ) {
+        $(this).addClass("future")
+        $(this).removeClass("past")
+        $(this).removeClass("present")
+
+    
+    
+    }
+    if (rowtime< cuurenttime) {
+        $(this).addClass("past")
+        $(this).removeClass("present")
+        $(this).removeClass("future")
+    }
+ $(this).children("button").on("click",function(event){
+    
+    var text= $(event.target).siblings("textarea").val()
+
+    
+    localStorage.setItem(rowtime,text)
+
+ })
+
+
+    console.log( index + ": " + $( this ).text() );
+  });
+
+
+
+
+
+
+
+
+
+
+}
+
+
+ 
+
+
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(function () {
+    
+    $(".saveBtn").on("click", function(){
+        console.log("click")
+        $("#nine").val()
+    })
+
+    $("<div>")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -10,6 +121,7 @@ $(function () {
     // useful when saving the description in local storage?
     //
     // TODO: Add code to apply the past, present, or future class to each time
+
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
